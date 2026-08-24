40a98822772baf99f1a8122f2c609ca95146db5c
victorruales1-gif <victorruales1@gmail.com>
2026-08-24 01:03:00 -0500

    Update image path in script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const starData = [
     label: "La primera vez que hablamos",
     date: "Mayo 2026",
     description: "Me asignaron una abogada para mi contratación y resultó ser la mujer más hermosa que había visto. Ese día no solo firmé un contrato... también me entregué sin darme cuenta. Fue la primera vez que hablamos, y desde entonces no he dejado de pensar en ti",
-    image: "assets/images/recuerdos/recuerdo1.jpg",
+    image: "images/recuerdos/recuerdo1.jpg",
   },
   // 1
   {
